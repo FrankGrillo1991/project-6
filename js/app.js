0f849880a0e86d3188bed52d31a241fc42043979
FrankGrillo1991 <fsg12491@aol.com>
2023-05-16 17:22:00 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-const phrases = ['Fantasia', 'Home Alone', 'The Shining', 'Amadeus', 'Immortal Beloved'];
+const phrases = ['Fantasia', 'Home Alone', 'The Shining', 'Amadeus', 'Immortal Beloved']
+
+const getRandomPhraseAsArray = arr => {
+
+}
+
+const addPhraseToDisplay = arr => {
+
+}
+
+const checkLetter = button => {
+
+}
+
+const checkWin = () => {
+
+}
+
+startButton.addEventListener('click', () => {
+
+});
+
+qwerty.addEventListener('click', e => {
+
+})
