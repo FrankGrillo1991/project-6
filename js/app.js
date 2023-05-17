b2af37616f8a591d311876950489043278941fcc
FrankGrillo1991 <fsg12491@aol.com>
2023-05-16 22:30:31 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const phrases = ['Fantasia', 'Home Alone', 'The Shining', 'Amadeus', 'Immortal Beloved']
 
-const querty = document.getElementById('querty');
+const querty = document.getElementById('qwerty');
 
 const phrase = document.getElementById('phrase');
 
