077085763a7edf44af07845fb6df5ed31c853f1d
FrankGrillo1991 <fsg12491@aol.com>
2023-05-30 00:45:43 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -64,6 +64,6 @@ startButton.addEventListener('click', () => {
 for (let i = 0; i < qwerty.length; i++) {
 
 qwerty.addEventListener('click', e => {
-    button[i].textContent = chosen.textContent.classList.add("chosen")
+    button[i].textContent = chosen[i].textContent.classList.add("chosen")
 })
 }
