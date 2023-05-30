8d92f524f38ed1e5358210a68203c376eb80f94c
FrankGrillo1991 <fsg12491@aol.com>
2023-05-30 19:37:05 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -65,6 +65,6 @@ startButton.addEventListener('click', () => {
 qwerty.addEventListener('click', (event) => {
     console.log(event.target)
     if (event.target.tagName === "BUTTON") {
-        button[i].classList.add("chosen")
+        event.target.tagName.classList.add("chosen")
     }
 })
