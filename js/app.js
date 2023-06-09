5bd5e3be27346953f0414e53ac12fbc3a6db2e2e
FrankGrillo1991 <fsg12491@aol.com>
2023-06-09 19:30:14 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ const checkLetter = button => {
     const characters = document.querySelectorAll(".letter");
     let match = null;
     for (let i = 0; i < characters.length; i++) {
-        if (button.textContent === characters[i].textContent) {
+        if (button.textContent === characters[i].textContent.toLowerCase()) {
          match = characters[i].textContent;
         characters[i].classList.add("show")
         console.log(characters[i].textContent)
